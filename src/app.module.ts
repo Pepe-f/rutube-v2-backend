d8d8b889d60aa6075ab76fb380f315000d302a41
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-04-03 01:30:55 +0300

    Add DB entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getTypeOrmConfig } from './config/typeorm.config';
+import { UserModule } from './user/user.module';
+import { VideoModule } from './video/video.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
@@ -13,6 +16,9 @@ import { getTypeOrmConfig } from './config/typeorm.config';
       inject: [ConfigService],
       useFactory: getTypeOrmConfig,
     }),
+    UserModule,
+    VideoModule,
+    CommentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
