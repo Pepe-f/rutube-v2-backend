912883e085e284d72a8ecfdb90a4764dae84cade
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-04-03 02:49:55 +0300

    Auth controller

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,7 +1,7 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class AuthDto {
-  @IsEmail()
+  @IsEmail({}, { message: 'Неверный формат Email' })
   email: string;
 
   @MinLength(6, { message: 'Не менее 6 символов' })
